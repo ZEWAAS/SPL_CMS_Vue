54abb4f78fec1e3a498ee833848b385a7ad3d712
ZEWAAS <simon.wallner@student.htldornbirn.at>
2025-10-20 09:21:45 +0200

    Class Car

---
+++ Typescript/Class_Constructor/Car.ts
@@ -0,0 +1,33 @@
+export class Car {
+    // Kapselung
+    // Objektvariablen, Instanzvariablen
+    // Gedächnisvariablen
+    private brand: string;
+    private model: string;
+    private year: number;
+
+    constructor(brand: string, model: string, year: number) {
+        this.brand = brand;
+        this.model = model;
+        this.checkYear(year);
+    }
+
+    public getYear() {
+        return this.year;
+    }
+
+    public setYear(year: number) {
+        this.checkYear(year);
+    }
+
+    private checkYear(year: number) {
+        if (year < 1886) {
+            //throw new Error("Year cannot be earlier than 1886.");
+            this.year = 1886;
+        } else if (year > 2025) {
+            //throw new Error("Year cannot be later than 2025.");
+            this.year = 2025;
+        } else {
+            this.year = year;
+        }
+}}
---
+++ Typescript/Class_Constructor/main.ts
@@ -0,0 +1,5 @@
+import { Car } from "./Car.ts";
+
+const myCar = new Car("Toyota", "Corolla", 2030);
+
+console.log(myCar);
